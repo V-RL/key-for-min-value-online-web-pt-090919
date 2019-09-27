31e305ac90a288e96361a4a1ce8b8e394934b9b6
Brandon Furr <brandon.furr123@gmail.com>
2019-09-27 13:35:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   low_value = Float::INFINITY
 name_hash.each do |key, value|
   if value < low_value 
-    return key
+    low_value = v 
+    low_key = k 
   end
   end
 end
